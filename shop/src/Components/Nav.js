fc201a2ba1124c6af7ca61bf1e244f745d3d7cd4
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-08-29 16:17:31 -0600

    api search working

--- shop/src/Components/Nav.js
+++ shop/src/Components/Nav.js
@@ -5,15 +5,31 @@ import logo2 from '../img/ShopifyLogo.png';
 
 import '../SASS/Nav.sass';
 
+// import {BrowserRouter as Router,Route, NavLink, Redirect} from 'react-router-dom';
 
-function Nav() {
-    return (
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <input placeholder="◎ Search for Movie Title"></input>
+class Nav extends Component {
 
-        </header>
-    );
+    constructor(props) {
+        super(props);
+        this.state = {
+         
+        }
+        }
+
+        componentDidMount() {
+
+        }
+
+        render() {
+          return (
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <input placeholder="◎ Search for Movie Title" onChange={this.props.inputHandler}></input>
+              <button onClick={this.props.searchMovies}>Search</button>
+            </header>
+        );
+        }
+
   }
   
   export default Nav;
