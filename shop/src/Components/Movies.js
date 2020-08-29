d66e9b262a3e8754f8266bb528e47167783a3596
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-08-29 17:50:59 -0600

    added nav arrows for search results

--- shop/src/Components/Movies.js
+++ shop/src/Components/Movies.js
@@ -20,22 +20,33 @@ class Movies extends Component {
 
     render() {
         let message;
+        let navarrows;
             if (this.props.movies.length === 0 && this.props.error === false) {
-                message = <h1 className='messageEmpty'>Search for a movie to nominate!</h1>
+                message = <h1 className='messageEmpty'>Search for a movie to nominate!</h1>;
+                navarrows = '';
             }
             else if (this.props.error === true) {
-                    message = <h1 className='messageInvalid'>There were no results, please search again!</h1>
+                    message = <h1 className='messageInvalid'>There were no results, please search again!</h1>;
+                    navarrows = '';
             }
             else {
                 message = '';
+                navarrows = <div className='nav-arrows'>
+                                <button class="arrow left">›</button>
+                                <h3>{this.props.page} of {this.props.totalPages}</h3>
+                                <button class="arrow right">›</button>
+                            </div>;
             }
 
         return (
-            <div className='moviesContainer'>
+            <div className='moviesComp'>
+                {navarrows}
                 {message}
+                <div className='moviesContainer'>
               {this.props.movies.map(movie => {
                   return <Movie movie={movie} />
               })}
+                </div>
             </div>
         )
     }
