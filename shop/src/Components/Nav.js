464094a821c8ea0058160673f397c409245df8e6
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-09-07 22:46:31 -0600

    added mobile layout

--- shop/src/Components/Nav.js
+++ shop/src/Components/Nav.js
@@ -10,7 +10,11 @@ import '../SASS/Nav.sass';
 // import {BrowserRouter as Router,Route, NavLink, Redirect} from 'react-router-dom';
 
 const Nav = (props) => {
-  const {isAuthenticated} = useAuth0()
+  const {loginWithRedirect, logout, user, isAuthenticated} = useAuth0()
+  
+  let logTop =   <div className='loginTop'>
+                  <h1 onClick={() => loginWithRedirect()}>Log in</h1>
+                </div>
 
     if (isAuthenticated) {
       let logIn = document.getElementsByClassName('loggedin')
@@ -18,12 +22,21 @@ const Nav = (props) => {
       for (let val of logIn) {
         val.style.visibility = 'visible'
       }
+      logTop = <div className='logoutTop'>
+      <h2>{user.name} |</h2>
+      <h1 onClick={() => logout()}>Log out</h1>
+    </div>
     }
           return (
             <header className="App-header">
+              <div className='logoGroup'>
               <img src={logo} className="App-logo" alt="logo" />
+              {logTop}
+              </div>
+              <div className='search'>
               <input className='loggedin' placeholder="◎ Search for Movie Title" onChange={props.inputHandler} onKeyDown={props.searchMoviesEnter}></input>
               <button className='loggedin' onClick={props.searchMovies}>Search</button>
+              </div>
             </header>
         );
   }
